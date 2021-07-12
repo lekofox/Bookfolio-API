da6c59a3624986efd1cf8c4129d37ba3b2a3989e
lekofox <leandro_dias9@hotmail.com>
2021-07-12 20:21:54 -0300

    adicao de migrations e models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,5 @@
 /* eslint-disable linebreak-style */
 import { Router } from 'express';
-import GetDoctorController from './app/controller/GetDoctorController';
-import PostDoctorController from './app/controller/PostDoctorController';
-import DeleteDoctorController from './app/controller/DeleteDoctorController';
-import PutDoctorController from './app/controller/PutDoctorController';
 
 const routes = new Router();
 
@@ -14,25 +10,6 @@ routes.get('/', (req, res) => {
   });
 });
 
-// POST Routes
-routes.post('/adiante-api/v1/medicos', PostDoctorController.store);
-routes.post('/adiante-api/v1/medicos/reativar/:crm', PostDoctorController.reactivateDoctor);
 
-// PUT Routes
-routes.put('/adiante-api/v1/medicos/:crm', PutDoctorController.update);
-
-// DELETE Routes
-routes.delete('/adiante-api/v1/medicos/:crm', DeleteDoctorController.delete);
-
-// GET Routes
-routes.get('/adiante-api/v1/medicos/CRM/:crm', GetDoctorController.getByCRM);
-routes.get('/adiante-api/v1/medicos/logradouro/:logradouro', GetDoctorController.getByAddress);
-routes.get('/adiante-api/v1/medicos/bairro/:bairro', GetDoctorController.getByDistrict);
-routes.get('/adiante-api/v1/medicos/cidade/:localidade', GetDoctorController.getByCity);
-routes.get('/adiante-api/v1/medicos/uf/:uf', GetDoctorController.getByState);
-routes.get('/adiante-api/v1/medicos/telefone/:telefone_fixo', GetDoctorController.getByPhone);
-routes.get('/adiante-api/v1/medicos/celular/:telefone_celular', GetDoctorController.getByCellPhone);
-routes.get('/adiante-api/v1/medicos/CEP/:CEP', GetDoctorController.getByCEP);
-routes.get('/adiante-api/v1/medicos/especialidade/:especialidade', GetDoctorController.getBySpecialization);
 
 export default routes;
