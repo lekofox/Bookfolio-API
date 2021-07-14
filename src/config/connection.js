452f3789bd777da57f5028c75b7db56731508564
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-07-14 13:35:02 -0300

    Criando conexão com os modelos utilizados (sem Foreign-keys)

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,11 +1,17 @@
 import { Sequelize } from 'sequelize';
 import database from './database';
-
+import Book from '../app/models/book';
+import BookGenre from '../app/models/bookgenre';
+import Genre from '../app/models/genre'
+import User from '../app/models/user';
 
 const connection = new Sequelize(database);
 
 // Model connections
-
+Book.init(connection)
+BookGenre.init(connection)
+Genre.init(connection)
+User.init(connection)
 // Associations between models
 
 export default connection;
