dd5d114a29fa01cf423a19281faa50298a6d2a1b
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-07-18 21:10:38 -0300

    Adicionando midlewares necessários para o funcionamento do passport, como o express-session e express flash, permitindo o armazenamento de sassão dentro do express, além disso adição das funções de start do passport permitindo seu funcionamento dentro do server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
+
 import express from 'express';
 import routes from './routes';
+import flash from 'express-flash'
+import session from 'express-session'
+import passport from 'passport';
 
 class App {
   constructor() {
@@ -10,6 +18,14 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(flash())
+    this.server.use(session({
+      secret: process.env.secret,
+      resave: false,
+      saveUninitialized: false
+    }))
+    this.server.use(passport.initialize())
+    this.server.use(passport.session())
   }
 
   routes() {
