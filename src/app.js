abc5c14b0a931572d264a68c6c3c499aa504338b
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-08-12 20:10:14 -0300

    adicionando header control

--- src/app.js
+++ src/app.js
@@ -21,7 +21,18 @@ class App {
 
   middlewares() {
     this.server.use(cors({origin: process.env.client,credentials: true,}))
-    this.server.use(express.json());
+    this.server.use(express.json())
+    this.server.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Credentials', true);
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+      if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+   });
     this.server.use(express.urlencoded({extended: true}))
     this.server.use(flash())
     this.server.use(cookieParser())
